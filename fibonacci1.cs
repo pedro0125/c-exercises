c8c961cae3db80215505b0fbf968897e14423ba2
pedro0125 <41745667+pedro0125@users.noreply.github.com>
2023-01-18 18:43:10 -0500

    Update fibonacci1.cs

--- fibonacci1.cs
+++ fibonacci1.cs
@@ -1,20 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 /**
- * Prints the firt 5 prime numbers from the Fibonacci series
+ * Prints the firt 10 prime numbers from the Fibonacci series
  * */
 namespace ConsoleApp1
 {
-    internal class Program
+    internal class fibonacci1
     {
         // IEnumerable<T> is an implementation of the Iterator Pattern in C#.
         private static IEnumerable<int> GetFibonacci()
         {
             int previousVal1 = 0, previousVal2 = 1;
+            yield return previousVal1;
+            yield return previousVal2;
 
             while (true)
             {
@@ -25,12 +27,23 @@ namespace ConsoleApp1
             }
         }
 
+        private static bool IsPrime(int x)
+        {
+            if (x < 2)
+                return false;
+            else if (x == 2)
+                return true;
+            else if (x % 2 == 0)
+                return false;
+
+            return Enumerable.Range(2, (int)Math.Sqrt(x) - 1).All(divisor => x % divisor != 0);
+        }
+
         static void Main(string[] args)
         {
-            IEnumerable<int> primes = GetFibonacci().Where(
-                n => Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(divisor => n % divisor != 0));
+            IEnumerable<int> primes = GetFibonacci().Where(IsPrime);
 
-            foreach (int num in primes.Take(5))
+            foreach (int num in primes.Take(10))
             {
                 Console.WriteLine(num);
             }
